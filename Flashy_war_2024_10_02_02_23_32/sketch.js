311c2ff71dc8adbf58f2ebba96e77d2911b660b4
dyom-creator <daya316@naver.com>
2024-10-02 11:35:52 +0900

    1st_re

---
+++ Flashy_war_2024_10_02_02_23_32/sketch.js
@@ -0,0 +1,21 @@
+function preload(){
+  
+}
+
+function setup() {
+  createCanvas(400, 400);
+  background(220);
+  ellipse(200, 200, 200, 200);
+  arc(200,200,100,100,0,PI);
+  point(150,180);
+  point(250,180);
+  line(180,200,220,200);
+  
+ 
+}
+
+function draw() {
+  //background(220);
+  //ellipse(200, 200, 200, 200);
+  //arc(200,200,100,100);
+}
